6a696e303ff0fb174a5181b360fc4e803115219f
Zachary Kuchar <zacharyk323@live.com>
2023-07-21 09:35:39 -0400

    Updated style

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Score(props) {
   return (
     <div className='score'>
-      <h2>{props.date}</h2>
-      <h3>{props.score}</h3>
+      <h2>Date: {props.date}</h2>
+      <h3>Score: {props.score}</h3>
     </div>
   )
 }
